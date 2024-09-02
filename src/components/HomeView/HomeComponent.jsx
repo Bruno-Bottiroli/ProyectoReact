b19d521179ea8ac5770cbe0bab24d60648b54b7f
Bruno-Bottiroli <bottirolibruno3@gmail.com>
2024-09-02 00:40:33 -0300

    .

--- src/components/HomeView/HomeComponent.jsx
+++ src/components/HomeView/HomeComponent.jsx
@@ -15,20 +15,20 @@ export default function HomeComponent() {
           <div className="row">
               <div className="col-12 d-flex">
                   <div className="col-6 tucan pe-2 image-container">
-                      <img className="tucan" src="public/imagenes/tucan.jpg" alt="" />
+                      <img className="tucan" src="/imagenes/tucan.jpg" alt="" />
                         <div className="overlay">
                           <a href="/Products" className="overlay-text">Tienda</a>
                         </div>
                   </div>
                   <div className="col-6">
                       <div className="col-12 trespalos image-container">
-                        <img className="w-100 trespalos" src="public/imagenes/trespalos.jpg" alt="" />
+                        <img className="w-100 trespalos" src="/imagenes/trespalos.jpg" alt="" />
                         <div className="overlay2">
                           <a href="/SobreNosotros" className="overlay-text">Sobre nosotros</a>
                         </div>
                       </div>
                       <div className="col-12 volar image-container">
-                        <img className="w-100 volar" src="public/imagenes/volar.jpg" alt="" />
+                        <img className="w-100 volar" src="/imagenes/volar.jpg" alt="" />
                         <div className="overlay2">
                           <a href="/Contact" className="overlay-text">Contacto</a>
                         </div>
